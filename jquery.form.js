267eca737400917dfd69f19e668f1ccc250acc28
Tom Chislett <tom@khaitu.com>
2011-05-20 16:50:10 +0100

    Fixed Internet Explorer bug when submitting a file upload and expecting JSON returned. Response Text now falls back to pre.innerHTML when pre.textContent is undefined

--- jquery.form.js
+++ jquery.form.js
@@ -376,7 +376,7 @@ $.fn.ajaxSubmit = function(options) {
 						var pre = doc.getElementsByTagName('pre')[0];
 						var b = doc.getElementsByTagName('body')[0];
 						if (pre) {
-							xhr.responseText = pre.textContent;
+							xhr.responseText = pre.textContent ? pre.textContent : pre.innerHTML;
 						}
 						else if (b) {
 							xhr.responseText = b.innerHTML;
