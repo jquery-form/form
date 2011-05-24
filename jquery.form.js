a640a11a987f95eb0974d8e7a7e82463ec78dd5c
malsup <malsup@gmail.com>
2011-05-23 20:20:25 -0400

    v2.78 version bump

--- jquery.form.js
+++ jquery.form.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery Form Plugin
- * version: 2.77 (23-MAY-2011)
+ * version: 2.78 (23-MAY-2011)
  * @requires jQuery v1.3.2 or later
  *
  * Examples and documentation at: http://malsup.com/jquery/form/
