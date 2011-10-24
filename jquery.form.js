afcf5339ab0bfd7e6710ae83b4defe1d55fbd7df
Perlover <perlover@perlover.com>
2011-10-24 22:46:12 +0600

    Done forgotten indent line

--- jquery.form.js
+++ jquery.form.js
@@ -346,7 +346,7 @@ $.fn.ajaxSubmit = function(options) {
 				if (!s.iframeTarget) {
 					// add iframe to doc and submit the form
 					$io.appendTo('body');
-			io.attachEvent ? io.attachEvent('onload', cb) : io.addEventListener('load', cb, false);
+					io.attachEvent ? io.attachEvent('onload', cb) : io.addEventListener('load', cb, false);
 				}
 				setTimeout(checkState,15);
 				form.submit();
