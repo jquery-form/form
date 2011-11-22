e3debb2c0bf198f45ce162099a0c57b87395e317
Yvan BARTHÉLEMY <ybarthelemy@direct-streams.com>
2011-11-22 10:53:43 +0100

    Support for meta csrf-param and csrf-token (Used by Rails 3).
    This adds an extra pair to submitted forms when both meta tags are present.

--- jquery.form.js
+++ jquery.form.js
@@ -289,6 +289,15 @@ $.fn.ajaxSubmit = function(options) {
 			return doc;
 		}
 		
+  	        // Rails CSRF hack
+	        var csrf_token = $('meta[name=csrf-token]').attr('content');
+	        var csrf_param = $('meta[name=csrf-param]').attr('content');
+
+	        if (csrf_param && csrf_token) {
+			s.extraData = s.extraData || {};
+	        	s.extraData[csrf_param] = csrf_token;
+		}
+
 		// take a breath so that pending repaints get some cpu time before the upload starts
 		function doSubmit() {
 			// make sure form attrs are set
