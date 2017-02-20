79efe0db8962cf86f13ad1be58de318f77f5257b
Kevin Morris <kmorri36@jhu.edu>
2017-02-20 17:13:43 -0500

    Moves JS file into 'src' folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,14 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
-  gulp.src(['jquery.form.js'])
+  gulp.src(['src/jquery.form.js'])
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(uglify({ preserveComments: 'some' }))
-    .pipe(concat('jquery.form.min.js'))
+    .pipe(concat('dist/jquery.form.min.js'))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('jquery.form.js', ['default']);
+  gulp.watch('src/jquery.form.js', ['default']);
 });
