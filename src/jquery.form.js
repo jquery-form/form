76a0b2a62b0fb66d2548949f7d10fb4acb592e60
Kevin Morris <kmorri36@jhu.edu>
2017-03-03 15:55:14 -0500

    Updates AMD definition and adds CommonJS

--- src/jquery.form.js
+++ src/jquery.form.js
@@ -10,21 +10,32 @@
  */
 /*global ActiveXObject */
 
-// AMD support
 (function (factory) {
-	"use strict";
-	if (typeof define === 'function' && define.amd) {
-		// using AMD; register as anon module
-		define(['jquery'], factory);
-	} if (typeof module !== 'undefined') {
-		factory(require('jquery'));
-	} else {
-		// no AMD; invoke directly
-		factory( (typeof(jQuery) != 'undefined') ? jQuery : window.Zepto );
-	}
-}
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function( root, jQuery ) {
+            if ( jQuery === undefined ) {
+                // require('jQuery') returns a factory that requires window to build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop if it's defined (how jquery works)
+                if ( typeof window !== 'undefined' ) {
+                    jQuery = require('jquery');
+                }
+                else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+            factory(jQuery);
+            return jQuery;
+        };
+    } else {
+        // Browser globals
+        factory(jQuery);
+    }
 
-(function($) {
+}(function ($) {
 "use strict";
 
 /*
@@ -1359,5 +1370,4 @@ function log() {
 		window.opera.postError(msg);
 	}
 }
-
 }));
