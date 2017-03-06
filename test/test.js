1a17a130d5ccaee00663fda17905cbb0367e73bc
Kevin Morris <kevinqmorris@gmail.com>
2017-03-05 21:04:03 -0500

    test: Adds test 'formToArray: "action" and "method" inputs'

--- test/test.js
+++ test/test.js
@@ -11,6 +11,15 @@ var arrayCount = function(arr, key) {
 	return count;
 };
 
+// helper method
+var arrayValue = function(arr, key) {
+	for (var i=0; i < arr.length; i++) {
+		if (arr[i].name === key) {
+			return arr[i].value;
+		}
+	}
+}
+
 
 //	var assert = require('chai').assert;
 var assert = chai.assert;
@@ -27,5 +36,12 @@ describe('form', function() {
 		assert.strictEqual(a.constructor, Array, 'type check');
 		assert.strictEqual(a.length, 13, 'array length');
 		assert.strictEqual(arrayCount(a, 'Multiple'), 3, 'multi-select');
+	});
+
+	it('formToArray: "action" and "method" inputs', function() {
+		var a = $('#form1').formToArray();
+		assert.strictEqual(a.constructor, Array, 'type check');
+		assert.strictEqual(arrayValue(a, 'action'), '1', 'input name=action');
+		assert.strictEqual(arrayValue(a, 'method'), '2', 'input name=method');
 	});
 });
