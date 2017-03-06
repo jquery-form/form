202d26e1da2224947fbc098f0e874945a4a7d099
Kevin Morris <kevinqmorris@gmail.com>
2017-03-05 20:27:57 -0500

    test: Runs real test
    http://malsup.com/jquery/form/test/

--- test/test.js
+++ test/test.js
@@ -3,22 +3,10 @@
 //	var assert = require('chai').assert;
 var assert = chai.assert;
 
-describe('Array', function() {
-	it('should start empty', function() {
-		var arr = [];
-
-		assert.equal(arr.length, 0);
-	});
-
-	it('stuff', function() {
-		var foo = 'bar'
-			,beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
-
-		assert.typeOf(foo, 'string'); // without optional message
-		assert.typeOf(foo, 'string', 'foo is a string'); // with optional message
-		assert.equal(foo, 'bar', 'foo equal `bar`');
-		assert.lengthOf(foo, 3, 'foo`s value has a length of 3');
-		assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
-		assert.equal(1, 1, 'Math works');
+describe('form', function() {
+	it('"action" and "method" form attributes', function() {
+		var f = $('#form1');
+		assert.strictEqual(f.attr('action'), 'text.php', 'form "action"');
+		assert.strictEqual(f.attr('method'), 'get', 'form "method"');
 	});
 });
