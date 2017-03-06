e4353c342ef6dbbeb09c2b85c488e21017a7b6c2
Kevin Morris <kevinqmorris@gmail.com>
2017-03-05 20:41:38 -0500

    test: Adds test 'formToArray: multi-select'

--- test/test.js
+++ test/test.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// helper method
+var arrayCount = function(arr, key) {
+	var count = 0;
+	for (var i=0; i < arr.length; i++) {
+		if (arr[i].name === key) {
+			count++;
+		}
+	}
+	return count;
+};
+
+
 //	var assert = require('chai').assert;
 var assert = chai.assert;
 
@@ -8,5 +20,12 @@ describe('form', function() {
 		var f = $('#form1');
 		assert.strictEqual(f.attr('action'), 'text.php', 'form "action"');
 		assert.strictEqual(f.attr('method'), 'get', 'form "method"');
+	});
+
+	it('formToArray: multi-select', function() {
+		var a = $('#form1').formToArray();
+		assert.strictEqual(a.constructor, Array, 'type check');
+		assert.strictEqual(a.length, 13, 'array length');
+		assert.strictEqual(arrayCount(a, 'Multiple'), 3, 'multi-select');
 	});
 });
